698f9ffc2a0bc3bf57e697b4255f9568070ed10e
r-serori <ryunosuke.karage@gmail.com>
2026-01-05 15:34:14 +0900

    ♻️環境変数の取得方法修正

--- src/providers/MSWProvider.tsx
+++ src/providers/MSWProvider.tsx
@@ -2,14 +2,12 @@
 
 import { useEffect, useState } from "react";
 
-import { env } from "@/lib/env";
-
 export function MSWProvider({ children }: { children: React.ReactNode }) {
   const [mswReady, setMswReady] = useState(false);
 
   useEffect(() => {
     const init = async () => {
-      if (env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+      if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
         const { worker } = await import("../mocks/browser");
         await worker.start({
           onUnhandledRequest: "bypass",
